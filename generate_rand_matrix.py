1bca5c71ff35242879a1188b63212ed97a642e32
anthonysalvatore <anthonyy.salvatore@gmail.com>
2022-10-04 20:14:03 +1100

    Update generate_rand_matrix.py
    uses random instead of numpy to generate matrix

--- generate_rand_matrix.py
+++ generate_rand_matrix.py
@@ -1,19 +1,18 @@
-import numpy as np
+import random
 
 
 def generate_matrix(matrix_id=0, size=(2, 2), max_val=9):
     row, col = size[0], size[1]
-    matrix = np.random.randint(1,max_val, size=(row, col))
     matrix_formatted = ''
     for r in range(row):
         matrix_formatted += f"{matrix_id}, {r}"
         for c in range(col):
-            matrix_formatted += f', {matrix[r][c]}'
+            matrix_formatted += f', {random.randint(1,max_val)}'
         matrix_formatted += '\n'
-    print(matrix)
     return matrix_formatted
 
-m = generate_matrix(3, (6, 6), 150)
+m = generate_matrix(3, (200, 200), 300000)
 with open('gen_matrixX.txt', 'w') as f:
     f.write(m)
+
 
