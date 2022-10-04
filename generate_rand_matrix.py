02f0ce10c08d68286c1f80752e7fe32bb64e2c8d
anthonysalvatore <anthonyy.salvatore@gmail.com>
2022-10-04 16:46:12 +1100

    mapper update
    mapper fixes, slight update to random matrix generator

--- generate_rand_matrix.py
+++ generate_rand_matrix.py
@@ -1,17 +1,19 @@
-import random
+import numpy as np
 
 
 def generate_matrix(matrix_id=0, size=(2, 2), max_val=9):
     row, col = size[0], size[1]
+    matrix = np.random.randint(1,max_val, size=(row, col))
     matrix_formatted = ''
     for r in range(row):
-        matrix_formatted += f"{matrix_id},{r}"
+        matrix_formatted += f"{matrix_id}, {r}"
         for c in range(col):
-            matrix_formatted += f',{random.randint(1,max_val)}'
+            matrix_formatted += f', {matrix[r][c]}'
         matrix_formatted += '\n'
+    print(matrix)
     return matrix_formatted
 
-
-m = generate_matrix(1, (5, 5))
-with open('gen_matrix.txt', 'w') as f:
+m = generate_matrix(3, (6, 6), 150)
+with open('gen_matrixX.txt', 'w') as f:
     f.write(m)
+
--- mapper.py
+++ mapper.py
@@ -12,22 +12,22 @@ n = int(sys.argv[2])  # cols in N
 for line in sys.stdin:
 
     line = line.strip()
-    field = line.split(", ")
+    field = line.split(",")
+    field = [f.strip() for f in field]  # if spaces between entries
 
-    matrix_num = int(field[0])  # which matrix
-    row = int(field[1])  # row val is index 1
+    matrix_num, row = int(field[0]), int(field[1])  # which matrix; row val is index 1
 
     for col, value in enumerate(field[2:]):
         # matrix M
         if matrix_num == 1:
             # generate number of key value pairs
-            for k in range(m):
+            for k in range(n):
                 print(f'{row},{k}\tM,{col},{value}')
 
         # matrix N
         elif matrix_num == 2:
             # generate number of key value pairs
-            for k in range(n):
+            for k in range(m):
                 print(f'{k},{col}\tN,{row},{value}')
 
         # matrix X
